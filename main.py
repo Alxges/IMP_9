12ff269f5973e20061742e895125a74d04ce42d0
alex <101549625+Alxges@users.noreply.github.com>
2022-11-18 08:25:48 +0000

    .

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-1+1=2
+import os
+import sys
+import time
+from csv import excel
+import numpy
+import numpy as np
+import pandas as pd
+
+
+excel_datei = pd.read_excel("Fragen.xlsx)
+
+print(excel_datei.iloc[5,5])
+
+
